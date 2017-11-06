8d91bbfedf5729ee2e468205a51442ea27e3629e
Maksym Kolesnykov <maxkolesnikov93@gmail.com>
2017-11-07 00:00:49 +0200

    latest additions

--- TodoListProject/assets/js/todos.js
+++ TodoListProject/assets/js/todos.js
@@ -1,11 +1,19 @@
-$("li").click(function () {
+$("ul").on("click", "li", function () {
     $(this).toggleClass("completed");
 });
 
-$("span").click(function (e) {
+$("ul").on("click", "span", function (e) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
 
     e.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(e) {
+    if(e.which == 13) {
+        var todoText = $(this).val();
+
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
 });
